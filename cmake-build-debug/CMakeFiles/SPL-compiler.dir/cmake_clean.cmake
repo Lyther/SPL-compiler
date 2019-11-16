4e6e520ab8bac520e01e436b20b5b514bd215f4a
Lyther <enderaoelyther@gmail.com>
2019-11-16 21:35:34 +0800

    project 1

---
+++ cmake-build-debug/CMakeFiles/SPL-compiler.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SPL-compiler.dir/AST.c.obj"
+  "SPL-compiler.pdb"
+  "SPL-compiler.exe"
+  "SPL-compiler.exe.manifest"
+  "libSPL-compiler.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/SPL-compiler.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
